5c54a1a5b0c7b4dee5444ab7c5257c94d31a74d5
Brandon Chinn <brandon@quizlet.com>
2016-04-05 17:53:59 -0700

    add methods to StudySet

--- StudySet.java
+++ StudySet.java
@@ -1,16 +1,39 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
 
 public class StudySet { 
 	private long id;
-	private List<StudySet> childSets;
+	private Set<StudySet> childSets;
 
 	public StudySet(long id) {
 		this.id = id;
-		this.childSets = new ArrayList();
+		this.childSets = new HashSet<>();
 	}
 
 	public long getId() {
 		return id;
+	}
+
+	public void addChild(StudySet studySet) {
+		childSets.add(studySet);
+	}
+
+	public Set<StudySet> getChildren() {
+		return childSets;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof StudySet)) {
+			return false;
+		}
+		return ((StudySet) obj).getId() == id;
+	}
+
+	@Override
+	public int hashCode() {
+		return (int) id;
 	}
 }
